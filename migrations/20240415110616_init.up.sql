20b99d13026aba712631c8f51897fbe4f3a08306
Lukasz Kielar <luk.kielar@gmail.com>
2024-04-16 12:47:53 +0000

    add TODO

--- migrations/20240415110616_init.up.sql
+++ migrations/20240415110616_init.up.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS messages (
     id TEXT NOT NULL PRIMARY KEY,
     role TEXT NOT NULL,
     content TEXT NOT NULL,
-    conversation_id TEXT NOT NULL,
-    FOREIGN KEY (conversation_id) REFERENCES conversations (id) ON DELETE CASCADE
+    conversation_id TEXT NOT NULL
 );
 CREATE INDEX idx_messages_conversation_id ON messages (conversation_id);

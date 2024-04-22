be00566c26311f77c46d498adf66cfb35505a73d
Lukasz Kielar <luk.kielar@gmail.com>
2024-04-22 09:58:08 +0000

    use maybesignal

--- src/app.rs
+++ src/app.rs
@@ -44,31 +44,34 @@ fn Sidebar() -> impl IntoView {
 }
 
 #[component]
-fn MessageComponent(message: ReadSignal<Message>) -> impl IntoView {
-    let is_user = move || match Role::from(message.get_untracked().role) {
+fn MessageComponent(message: MaybeSignal<Message>) -> impl IntoView {
+    let message = message.get();
+    let message_role = message.role;
+    let message_content = message.content;
+
+    let is_user = match Role::from(message_role) {
         Role::Assistant | Role::System => false,
         Role::User => true,
     };
 
-    let message_class = move || {
-        let message_role_class = match is_user() {
+    let message_class = {
+        let message_role_class = match is_user {
             true => "dark:bg-gray-800",
             false => "bg-gray-50 dark:bg-[#444654]",
         };
         format!("group w-full text-gray-800 dark:text-gray-100 border-b border-black/10 dark:border-gray-900/50 {message_role_class}")
     };
 
-    let message_icon = move || {
+    let message_icon = {
         let class = "h-4 w-4 text-white";
-        match is_user() {
+        match is_user {
             true => view! { <Icon icon=icondata::LuUser class=class/> },
             false => view! { <Icon icon=icondata::LuBot class=class/> },
         }
     };
 
-    let message_content_class = move || {
-        let message_content = message.get_untracked().content;
-        if !is_user() && message_content == "" {
+    let message_content_class = {
+        if !is_user && message_content == "" {
             view! { <Icon icon=icondata::LuTextCursorInput class="h-6 w-6 animate-pulse"/> }
                 .into_view()
         } else {
@@ -77,12 +80,12 @@ fn MessageComponent(message: ReadSignal<Message>) -> impl IntoView {
     };
 
     view! {
-        <div class=message_class()>
+        <div class=message_class>
             <div class="text-base gap-4 md:gap-6 md:max-w-2xl lg:max-w-xl xl:max-w-3xl flex lg:px-0 m-auto w-full">
                 <div class="flex flex-row gap-4 md:gap-6 md:max-w-2xl lg:max-w-xl xl:max-w-3xl p-4 md:py-6 lg:px-0 m-auto w-full">
                     <div class="w-8 flex flex-col relative items-end">
                         <div class="relative h-7 w-7 p-1 rounded-full text-white flex items-center justify-center bg-black/75 text-opacity-100r">
-                            {message_icon()}
+                            {message_icon}
                         </div>
                         <div class="text-xs flex items-center justify-center gap-1 absolute left-0 top-2 -ml-4 -translate-x-full group-hover:visible !invisible">
                             <button disabled class="text-gray-300 dark:text-gray-400"></button>
@@ -94,7 +97,7 @@ fn MessageComponent(message: ReadSignal<Message>) -> impl IntoView {
                         <div class="flex flex-grow flex-col gap-3">
                             <div class="min-h-10 flex flex-col items-start gap-4 whitespace-pre-wrap break-words">
                                 <div class="markdown prose w-full break-words dark:prose-invert dark">
-                                    {message_content_class()}
+                                    {message_content_class}
                                 </div>
                             </div>
                         </div>
@@ -162,8 +165,8 @@ fn Conversation() -> impl IntoView {
         <div class="flex max-w-full flex-1 flex-col">
             <div class="relative h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1">
                 <div class="flex-1 overflow-hidden">
-                    <div class="react-scroll-to-bottom--css-ikyem-79elbk h-full dark:bg-gray-800">
-                        <div class="react-scroll-to-bottom--css-ikyem-1n7m0yu">
+                    <div class="scroll-to-bottom--css-ikyem-79elbk h-full dark:bg-gray-800">
+                        <div class="scroll-to-bottom--css-ikyem-1n7m0yu">
                             <div class="flex flex-col items-center text-sm bg-gray-800">
                                 <Transition fallback=move || {
                                     view! {
@@ -182,8 +185,7 @@ fn Conversation() -> impl IntoView {
                                         messages()
                                             .iter()
                                             .map(|m| {
-                                                let (message, _) = create_signal(m.clone());
-                                                view! { <MessageComponent message/> }
+                                                view! { <MessageComponent message=m.to_owned().into()/> }
                                             })
                                             .collect_view()
                                     }}

a7b7decf1634c080dfad8a2d53c6780c6e368a8d
Lukasz Kielar <luk.kielar@gmail.com>
2024-04-23 12:09:02 +0000

    define routes

--- src/app.rs
+++ src/app.rs
@@ -2,7 +2,7 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
-use crate::frontend::components::{Conversation, Sidebar};
+use crate::frontend::views::{Home, NotFound};
 
 #[component]
 pub fn App() -> impl IntoView {
@@ -13,14 +13,13 @@ pub fn App() -> impl IntoView {
         <Title text="Welcome to LokAI!"/>
         <Stylesheet id="leptos" href="/pkg/lokai.css"/>
         <Meta charset="UTF-8"/>
-
-        <main class="overflow-hidden w-full h-screen relative flex">
-            <div class="dark hidden flex-shrink-0 bg-gray-900 md:flex md:w-[260px] md:flex-col">
-                <div class="flex h-full min-h-0 flex-col ">
-                    <Sidebar/>
-                </div>
-            </div>
-            <Conversation/>
-        </main>
+        <Router>
+            <main>
+                <Routes>
+                    <Route path="/" view=Home/>
+                    <Route path="/*any" view=NotFound/>
+                </Routes>
+            </main>
+        </Router>
     }
 }

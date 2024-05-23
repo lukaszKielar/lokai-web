18d3f240fb5ab8566ded43793e638cd4a53d42e5
Lukasz Kielar <luk.kielar@gmail.com>
2024-05-23 12:13:27 +0000

    remove leptos

--- src/frontend/components/mod.rs
+++
@@ -1,7 +0,0 @@
-mod message;
-mod prompt;
-mod sidebar;
-
-pub(crate) use message::Messages;
-pub(crate) use prompt::Prompt;
-pub(crate) use sidebar::Sidebar;
--- src/server/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod api;
-#[cfg(feature = "ssr")]
-pub mod db;
-#[cfg(feature = "ssr")]
-pub mod error;
-#[cfg(feature = "ssr")]
-pub mod ollama;
